7008dfe9b9bdd3b3bf62494ef01b80e7a2b78d15
pauloeddias <pauloeddias@gmail.com>
2021-03-12 11:26:26 -0300

    feature: Months to goal percentage
    Added the plot to show how long it will take to reach the desired body fat percentage based on the previous 30 days data

--- StaticFunctions.py
+++ StaticFunctions.py
@@ -2,10 +2,12 @@ import os
 
 import gspread
 import pandas as pd
+from sklearn.linear_model import LinearRegression
+import numpy as np
 from oauth2client.service_account import ServiceAccountCredentials
 
 
-def load_data():
+def load_data() -> pd.DataFrame:
     scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
 
     creds = ServiceAccountCredentials.from_json_keyfile_name(os.environ['WEIGHT_DASHBOARD_CREDENTIALS_PATH'], scope)
@@ -26,5 +28,25 @@ def load_data():
     df['muscle_percentage'] = df['Muscle Percentage'].astype('float64')
     df['root_metabolism'] = df['Root Metabolism'].astype('float64')
     df = df[['timestamp', 'weight', 'fat_percentage', 'muscle_percentage', 'root_metabolism']]
+    df.set_index('timestamp', inplace=True)
 
+    return df
+
+
+def resample_every_day(df: pd.DataFrame) -> pd.DataFrame:
+    d2 = df.resample('D').first()
+    d2.fillna(method='ffill', inplace=True)
+    return d2
+
+
+def get_months_to_goal_percentage(column: pd.Series,percentage:float) -> float:
+    # percentage = 15
+    model = LinearRegression()
+    model.fit(X=np.arange(len(column)).reshape(-1,1),y=column.values.reshape(-1,1))
+    x = (percentage-model.intercept_)/model.coef_[0]
+    return x/30
+
+def months_to_goal_fat_percentage(goal_percentage: float, df: pd.DataFrame) -> pd.DataFrame:
+    df['months_to_goal_percentage'] = df['fat_percentage'].rolling(30,min_periods=30).apply(get_months_to_goal_percentage,args=tuple([goal_percentage]))
+    df = df.loc[abs(df['months_to_goal_percentage'])<=20.0]
     return df
--- app.py
+++ app.py
@@ -2,7 +2,8 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import plotly.graph_objects as go
-from StaticFunctions import load_data
+
+from StaticFunctions import *
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
@@ -10,19 +11,34 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 df = load_data()
 
+d2 = resample_every_day(df)
+d2 = months_to_goal_fat_percentage(goal_percentage=8, df=d2)
+
 fig = go.Figure()
-fig.add_trace(go.Scatter(x=df['timestamp'],y=df['fat_percentage'],mode='markers'))
+fig.add_trace(go.Scatter(x=df.index, y=df['fat_percentage'], mode='markers'))
 fig.update_layout(
     title="Fat Percentage",
     xaxis_title="Date",
     yaxis_title="Fat Percentage"
 )
 
+f2 = go.Figure()
+f2.add_trace(go.Scatter(x=d2.index, y=d2['months_to_goal_percentage'], mode='markers'))
+f2.update_layout(
+    title="Months To Goal Percentage",
+    xaxis_title="Date",
+    yaxis_title="Months To Goal Percentage"
+)
+
 app.layout = html.Div(children=[
     html.H1(children='Body Measurements'),
+    dcc.Graph(
+        id='months_fat',
+        figure=f2
+    ),
 
     dcc.Graph(
-        id='example-graph',
+        id='fat_percentage',
         figure=fig
     )
 ])
