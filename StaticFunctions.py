a35041b541fef9bd83d6588650182a19ac0d51d7
pauloeddias <pauloeddias@gmail.com>
2021-03-12 10:26:40 -0300

    refactor: Getting the data through gspread
    Change in how the Google Sheet was acquired, now using gspread

--- StaticFunctions.py
+++ StaticFunctions.py
@@ -1,28 +1,30 @@
+import os
+
+import gspread
 import pandas as pd
+from oauth2client.service_account import ServiceAccountCredentials
 
 
 def load_data():
-    df = pd.read_html(
-        'https://docs.google.com/spreadsheets/d/1UdohGMcpFfyhzNRt2qo6Jngj0t_yF5hOXL83TQcEAU8/edit?usp=sharing')
-    df = df[0][['A', 'B', 'C', 'D', 'E']]
-    data_hora = df['A'].tolist()[5:]
-    weight = df['B'].tolist()[5:]
-    fat_perc = df['C'].tolist()[5:]
-    musc_perc = df['D'].tolist()[5:]
-    rm = df['E'].tolist()[5:]
+    scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
 
-    df = pd.DataFrame(
-        {
-            'timestamp': data_hora,
-            'weight': weight,
-            'fat_percentage': fat_perc,
-            'muscle_percentage': musc_perc,
-            'root_metabolism': rm
-        },
-    )
-    df['timestamp'] = pd.to_datetime(df['timestamp'])
-    df['weight'] = df['weight'].astype('float64')
-    df['fat_percentage'] = df['fat_percentage'].astype('float64')
-    df['muscle_percentage'] = df['muscle_percentage'].astype('float64')
-    df['root_metabolism'] = df['root_metabolism'].astype('float64')
+    creds = ServiceAccountCredentials.from_json_keyfile_name(os.environ['WEIGHT_DASHBOARD_CREDENTIALS_PATH'], scope)
+
+    client = gspread.authorize(creds)
+    sheet = client.open('Body Index')
+
+    sheet_instance = sheet.get_worksheet(0)
+
+    data = sheet_instance.get_all_values()
+    headers = data.pop(0)
+
+    df = pd.DataFrame(data, columns=headers)
+    df = df.iloc[4:, :]
+    df['timestamp'] = pd.to_datetime(df['Carimbo de data/hora'])
+    df['weight'] = df['Weight'].astype('float64')
+    df['fat_percentage'] = df['Fat Percentage'].astype('float64')
+    df['muscle_percentage'] = df['Muscle Percentage'].astype('float64')
+    df['root_metabolism'] = df['Root Metabolism'].astype('float64')
+    df = df[['timestamp', 'weight', 'fat_percentage', 'muscle_percentage', 'root_metabolism']]
+
     return df
