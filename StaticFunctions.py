cb69a1517a659dbd1f727bc7f35bddded6f1efc5
pauloeddias <pauloeddias@gmail.com>
2021-03-12 14:45:48 -0300

    feature: Callbacks to update figures
    Added a datepicker and the callback to update the figures when the selected date changes

--- StaticFunctions.py
+++ StaticFunctions.py
@@ -1,10 +1,11 @@
 import os
 
 import gspread
-import pandas as pd
-from sklearn.linear_model import LinearRegression
 import numpy as np
+import pandas as pd
+import plotly.graph_objects as go
 from oauth2client.service_account import ServiceAccountCredentials
+from sklearn.linear_model import LinearRegression
 
 
 def load_data() -> pd.DataFrame:
@@ -25,9 +26,12 @@ def load_data() -> pd.DataFrame:
     df['timestamp'] = pd.to_datetime(df['Carimbo de data/hora'])
     df['weight'] = df['Weight'].astype('float64')
     df['fat_percentage'] = df['Fat Percentage'].astype('float64')
+    df['fat_mass'] = df['weight'] * df['fat_percentage'] / 100
     df['muscle_percentage'] = df['Muscle Percentage'].astype('float64')
+    df['muscle_mass'] = df['weight'] * df['muscle_percentage'] / 100
     df['root_metabolism'] = df['Root Metabolism'].astype('float64')
-    df = df[['timestamp', 'weight', 'fat_percentage', 'muscle_percentage', 'root_metabolism']]
+    df = df[
+        ['timestamp', 'weight', 'fat_percentage', 'muscle_percentage', 'root_metabolism', 'muscle_mass', 'fat_mass']]
     df.set_index('timestamp', inplace=True)
 
     return df
@@ -39,14 +43,28 @@ def resample_every_day(df: pd.DataFrame) -> pd.DataFrame:
     return d2
 
 
-def get_months_to_goal_percentage(column: pd.Series,percentage:float) -> float:
+def get_months_to_goal_percentage(column: pd.Series, percentage: float) -> float:
     # percentage = 15
     model = LinearRegression()
-    model.fit(X=np.arange(len(column)).reshape(-1,1),y=column.values.reshape(-1,1))
-    x = (percentage-model.intercept_)/model.coef_[0]
-    return x/30
+    model.fit(X=np.arange(len(column)).reshape(-1, 1), y=column.values.reshape(-1, 1))
+    x = (percentage - model.intercept_) / model.coef_[0]
+    return x / 30
+
 
 def months_to_goal_fat_percentage(goal_percentage: float, df: pd.DataFrame) -> pd.DataFrame:
-    df['months_to_goal_percentage'] = df['fat_percentage'].rolling(30,min_periods=30).apply(get_months_to_goal_percentage,args=tuple([goal_percentage]))
-    df = df.loc[abs(df['months_to_goal_percentage'])<=20.0]
+    df['months_to_goal_percentage'] = df['fat_percentage'].rolling(30, min_periods=30).apply(
+        get_months_to_goal_percentage, args=tuple([goal_percentage]))
+    df = df.loc[abs(df['months_to_goal_percentage']) <= 20.0]
     return df
+
+
+def make_line_plot(x, y, title, xaxis_title, yaxis_title, mode='markers') -> go.Figure():
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=x, y=y, mode=mode))
+    fig.update_layout(
+        title=title,
+        xaxis_title=xaxis_title,
+        yaxis_title=yaxis_title,
+        transition_duration=500
+    )
+    return fig
--- app.py
+++ app.py
@@ -1,7 +1,9 @@
+from datetime import datetime
+
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-import plotly.graph_objects as go
+from dateutil import relativedelta
 
 from StaticFunctions import *
 
@@ -10,38 +12,92 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 df = load_data()
+date_min = df.index[0]
+date_max = df.index[-1]
 
 d2 = resample_every_day(df)
 d2 = months_to_goal_fat_percentage(goal_percentage=8, df=d2)
 
-fig = go.Figure()
-fig.add_trace(go.Scatter(x=df.index, y=df['fat_percentage'], mode='markers'))
-fig.update_layout(
-    title="Fat Percentage",
-    xaxis_title="Date",
-    yaxis_title="Fat Percentage"
-)
-
-f2 = go.Figure()
-f2.add_trace(go.Scatter(x=d2.index, y=d2['months_to_goal_percentage'], mode='markers'))
-f2.update_layout(
-    title="Months To Goal Percentage",
-    xaxis_title="Date",
-    yaxis_title="Months To Goal Percentage"
-)
+fat_percentage = make_line_plot(x=df.index, y=df['fat_percentage'], title='Fat Percentage', xaxis_title='Date',
+                                yaxis_title="Fat Percentage")
+muscle_percentage = make_line_plot(x=df.index, y=df['muscle_percentage'], title='Muscle Percentage', xaxis_title='Date',
+                                   yaxis_title="Muscle Percentage")
+months_to_goal_percentage = make_line_plot(x=d2.index, y=d2['months_to_goal_percentage'], mode='markers',
+                                           title='Months To Goal Percentage', xaxis_title='Date',
+                                           yaxis_title="Months To Goal Percentage")
+fat_mass = make_line_plot(x=df.index, y=df['fat_mass'], title='Fat Mass', xaxis_title='Date',
+                          yaxis_title="Fat Mass (Kg)")
+muscle_mass = make_line_plot(x=df.index, y=df['muscle_mass'], title='Muscle Mass', xaxis_title='Date',
+                             yaxis_title="Muscle Mass (Kg)")
 
 app.layout = html.Div(children=[
-    html.H1(children='Body Measurements'),
-    dcc.Graph(
-        id='months_fat',
-        figure=f2
+    html.H1(children='Body Measurements',
+            style={'display': 'flex', 'flex-direction': 'row', 'justify-content': 'center'}),
+    dcc.DatePickerRange(
+        id='my-date-picker-range',
+        min_date_allowed=date_min,
+        max_date_allowed=date_max,
+        initial_visible_month=datetime.now() - relativedelta.relativedelta(months=3),
+        start_date=datetime.now() - relativedelta.relativedelta(months=3),
+        end_date=date_max,
+        style={'display': 'flex', 'flex-direction': 'row', 'justify-content': 'center'}
     ),
+    html.Div([
+        dcc.Graph(
+            id='fat_percentage',
+            figure=fat_percentage
+        ),
+        dcc.Graph(
+            id='muscle_percentage',
+            figure=muscle_percentage
+        ),
 
-    dcc.Graph(
-        id='fat_percentage',
-        figure=fig
-    )
-])
+        dcc.Graph(
+            id='fat_mass',
+            figure=fat_mass
+        ),
+        dcc.Graph(
+            id='muscle_mass',
+            figure=muscle_mass
+        ),
+        dcc.Graph(
+            id='months_to_goal_percentage',
+            figure=months_to_goal_percentage,
+            style={'display': 'flex', 'flex-direction': 'row', 'justify-content': 'center'}
+        )
+    ],
+        style={'display': 'flex', 'flex-direction': 'column', 'justify-content': 'center'}
+    )])
+
+
+@app.callback(
+    [dash.dependencies.Output('fat_percentage', 'figure'),
+     dash.dependencies.Output('fat_mass', 'figure'),
+     dash.dependencies.Output('muscle_percentage', 'figure'),
+     dash.dependencies.Output('muscle_mass', 'figure'),
+     dash.dependencies.Output('months_to_goal_percentage', 'figure')],
+    [dash.dependencies.Input('my-date-picker-range', 'start_date'),
+     dash.dependencies.Input('my-date-picker-range', 'end_date')])
+def update_figure(start_date=date_min, end_date=date_max):
+    filtered_df = df[(df.index >= start_date) & (df.index <= end_date)]
+    month_df = d2[(d2.index >= start_date) & (d2.index <= end_date)]
+
+    fp = make_line_plot(x=filtered_df.index, y=filtered_df['fat_percentage'], title='Fat Percentage',
+                        xaxis_title='Date',
+                        yaxis_title="Fat Percentage")
+    mp = make_line_plot(x=filtered_df.index, y=filtered_df['muscle_percentage'], title='Muscle Percentage',
+                        xaxis_title='Date',
+                        yaxis_title="Muscle Percentage")
+    fm = make_line_plot(x=filtered_df.index, y=filtered_df['fat_mass'], title='Fat Mass', xaxis_title='Date',
+                        yaxis_title="Fat Mass (Kg)")
+    mm = make_line_plot(x=filtered_df.index, y=filtered_df['muscle_mass'], title='Muscle Mass', xaxis_title='Date',
+                        yaxis_title="Muscle Mass (Kg)")
+    mtgp = make_line_plot(x=month_df.index, y=month_df['months_to_goal_percentage'], mode='markers',
+                          title='Months To Goal Percentage', xaxis_title='Date',
+                          yaxis_title="Months To Goal Percentage")
+
+    return fp, fm, mp, mm, mtgp
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
